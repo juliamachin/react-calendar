2464633913602f585cc35d6919c4880a8fdcf5cf
juliamachin <juliaamachin@gmail.com>
2023-02-02 19:31:02 -0800

    Set up components, installations, and calendar

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import "./Home.css";
+import Calendar from "react-calendar";
+
+const Home = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default Home
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import "./Nav.css"
+
+const Nav = () => {
+    return (
+        <div>
+            <h2>dis NavBar</h2>
+        </div>
+    )
+}
+
+export default Nav
